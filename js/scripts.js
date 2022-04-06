23e13bcdca2f4517a444ee2beee83fb672216417
Adrienne Matosich <amatosich@gmail.com>
2022-04-06 13:53:03 -0700

    change survey input types.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const help = $("input:radio[name=help]:checked").va();
+    const date = $("#start").val();
+    const favoriteColor = $("#color").val();
+  })
+}
